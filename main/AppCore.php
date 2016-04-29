66de5b22a12a05a9a5d50a8347a372c7a32beee0
mycobiont <eric@theothersideofthenet.com>
2016-04-29 06:50:55 -0700

    ok

---
+++ main/AppCore.php
@@ -0,0 +1,13 @@
+<?php
+
+class AppCore {
+// Constructor is to handle security including early 
+// sanitation of URL arguements and user access identification
+// after that, arguements will be eliminated so are only
+// accessible in the sanitized form, and all functions
+// will be made with proper user context.
+// Sets will need to specifically establish user access level
+// or will not be visible at all.
+
+
+ } // end class AppCore
