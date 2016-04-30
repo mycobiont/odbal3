834b00aac59d0bdec339ec442334a8df09dda8ab
mycobiont <eric@theothersideofthenet.com>
2016-04-30 11:19:16 -0700

    ok

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-include "./main/AppCore.php";
+// A couple things to be aware of, for security:
+// 1. URL arguements are passed to a variable in the App class
+//    with sanitation by mysqli real_escape_string, then eliminated
+//    from $_GET (replaced with "prohibited".
+// 2. The application uses prepared statements for a second protection
+//    from sql injection; any custom programming should do the same.
 
+include "./main/AppCore.php";
 $App = new AppCore("../odbal3.conf");
  
--- main/AppCore.php
+++ main/AppCore.php
@@ -1,16 +1,41 @@
 <?php
 
 class AppCore {
-// Constructor is to handle security including early 
-// sanitation of URL arguements and user access identification
-// after that, arguements will be eliminated so are only
-// accessible in the sanitized form, and all functions
-// will be made with proper user context.
-// Sets will need to specifically establish user access level
-// or will not be visible at all.
-
+ // Constructor is to handle security including early 
+ // sanitation of URL arguements and user access identification
+ // after that, arguements will be eliminated so are only
+ // accessible in the sanitized form, and all functions
+ // will be made with proper user context.
+ // Sets will need to specifically establish user access level
+ // or will not be visible at all.
+ public $URLargs;
+ private $DBconn1;
+ private $DBconn2;
+ 
  function __construct($primaryConf) {
-  echo $primaryConf;
+  echo "Loading...<br />";
+  include $primaryConf;
+  $this->DBconn1 = new mysqli($host, $user_guest, $pass_guest, $db);
+  if (mysqli_connect_errno()) {
+   echo("Connect failed: " . mysqli_connect_error());
+   exit();
+   }
+  if (!$this->DBconn1->set_charset("utf8")) {
+   // see https://www.toptal.com/php/a-utf-8-primer-for-php-and-mysql
+   printf("Error loading character set utf8: %s\n", $mysqli->error);
+   exit();
+   }
+  $this->URLargs = $this->SanitizeURL(); 
+  echo print_r($this->URLargs, true);
+  }
+  
+ function SanitizeURL() {
+  foreach ($_GET as $key => $value) {
+   $a[$key] = $this->DBconn1->real_escape_string($value);
+   $_GET[$key] = "prohibited";
+   // echo $value . '<br />';
+   }
+  return $a;
   }
  
  
