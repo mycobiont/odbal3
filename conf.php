1bea53aa578b491a5487562a440ad8279104e425
mycobiont <eric@theothersideofthenet.com>
2016-05-01 16:57:22 -0700

    ok

---
+++ conf.php
@@ -0,0 +1,20 @@
+<?php
+// Making this a standard php file makes it more difficult
+// for hackers to obtain the passwords
+
+// The first secion is for the app to communicate with the database
+$db = "";
+$host = "localhost";
+$user_guest = "";
+$pass_guest = "";
+$user_editor = "";
+$pass_editor = "";
+
+// These should be commended out and the values removed 
+// after database setup.
+// These provide the administrative user and password for 
+// establishing the App.
+$setup = "true";
+$user_admin = "";
+$pass_admin = "";
+
