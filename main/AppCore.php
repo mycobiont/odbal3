a93bfb5519df47f67b1a7ee05454b9042dab60ec
mycobiont <eric@ubuntu>
2016-05-01 21:37:19 -0700

    log incoming requests

--- main/AppCore.php
+++ main/AppCore.php
@@ -15,42 +15,28 @@ class AppCore {
  private $DBconn2;
  
  function __construct($primaryConf) {
-  echo "Loading...<br />";
-  include $primaryConf;
-  $testconn = new mysqli($host, $user_guest, $pass_guest);
-  if(!$q = $testconn->prepare("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?")) {
-   echo "Error. Contact administrator.";
-   exit();
+   //Absolute first thing... log the URL request with IP
+   echo 'a';
+   if(!logURLrequest()) {
+     echo "Internal logging error.";
+     exit();
    }
-  if(!$q->bind_param("s", $db)) {
-   echo "Error. Contact administrator.";
-   exit();   
+   echo "Loading...<br />";
+   include $primaryConf;
+   $this->URLargs = $this->SanitizeURL(); 
+   echo print_r($this->URLargs, true);
+   $this->DBconn1 = new mysqli($host, $user_guest, $pass_guest, $db);
+   if (mysqli_connect_errno()) {
+     // if database doesn't exist, then head into setup
+     echo("Connect failed: " . mysqli_connect_errno()); //mysqli_connect_error());
+     exit();
    }
-  if(!$q->execute()) {
-   echo "Error. Contact administrator.";
-   exit();   
+   if (!$this->DBconn1->set_charset("utf8")) {
+     // see https://www.toptal.com/php/a-utf-8-primer-for-php-and-mysql
+     printf("Error loading character set utf8: %s\n", $mysqli->error);
+     exit();
    }
-   $result = $q->get_result();
-   while ($row = $result->fetch_array(MYSQLI_NUM)) {
-    foreach ($row as $r) {
-     print "$r ";
-     }
-    print "\n";
-    }
-  $this->DBconn1 = new mysqli($host, $user_guest, $pass_guest, $db);
-  if (mysqli_connect_errno()) {
-   // if database doesn't exist, then head into setup
-   echo("Connect failed: " . mysqli_connect_errno()); //mysqli_connect_error());
-   exit();
-   }
-  if (!$this->DBconn1->set_charset("utf8")) {
-   // see https://www.toptal.com/php/a-utf-8-primer-for-php-and-mysql
-   printf("Error loading character set utf8: %s\n", $mysqli->error);
-   exit();
-   }
-  $this->URLargs = $this->SanitizeURL(); 
-  echo print_r($this->URLargs, true);
-  }
+ }
   
  function SanitizeURL() {
   foreach ($_GET as $key => $value) {
